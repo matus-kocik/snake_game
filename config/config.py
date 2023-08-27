acaf22d79f8dbede1b25508d7b8b5cecbec34401
Matúš Kočik <matuskocik@gmail.com>
2023-08-27 18:18:25 +0200

    create snake as rectangel

--- config/config.py
+++ config/config.py
@@ -1,1 +1,6 @@
+import pygame
+BODY_COLOR = pygame.Color(235, 128, 52)
+
 GAME_RES = (600, 600)
+
+SNAKE_SIZE = 10
--- game.py
+++ game.py
@@ -5,10 +5,14 @@ from config import config
 if __name__ == "__main__":
     pygame.init()
     window = pygame.display.set_mode(config.GAME_RES)
+    snake = [config.GAME_RES[0] // 2, config.GAME_RES[1] // 2]
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys,exit()
+                
+        pygame.draw.rect(window, config.BODY_COLOR, pygame.Rect(snake[0], snake[1], config.SNAKE_SIZE, config.SNAKE_SIZE))
+        
         pygame.display.update()
