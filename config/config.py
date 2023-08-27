a1ebdaa6f3fb5dbb24147800be2fc7c483c94cd6
Matúš Kočik <matuskocik@gmail.com>
2023-08-27 18:58:12 +0200

    add function def update_position and by every iteration for direction UP

--- config/config.py
+++ config/config.py
@@ -1,6 +1,7 @@
 import pygame
 
 BODY_COLOR = pygame.Color(235, 128, 52)
+BACKGROUND_COLOR = pygame.Color(0, 0, 0)
 
 GAME_RES = (600, 600)
 GAME_FPS = 10
--- game.py
+++ game.py
@@ -2,19 +2,27 @@ import pygame
 import sys
 from config import config
 
+def update_position(snake, direction):
+    if direction == "UP":
+        snake = [snake[0], snake[1] -1]
+    return snake
+
 if __name__ == "__main__":
     pygame.init()
     clock = pygame.time.Clock()
     window = pygame.display.set_mode(config.GAME_RES)
     snake = [config.GAME_RES[0] // 2, config.GAME_RES[1] // 2]
+    direction = "UP"
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys,exit()
-                
+        
+        snake = update_position(snake, direction)        
         pygame.draw.rect(window, config.BODY_COLOR, pygame.Rect(snake[0], snake[1], config.SNAKE_SIZE, config.SNAKE_SIZE))
         
         pygame.display.update()
+        window.fill(config.BACKGROUND_COLOR)
         clock.tick(config.GAME_FPS)
